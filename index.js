3f3635a890a744845308683cb89a6332fbdf5ba3
csmathuria123 <csmathuria03@gmail.com>
2019-02-17 22:13:30 +0530

    small changes

--- index.js
+++ index.js
@@ -83,6 +83,12 @@ x.controller("workshops",function($scope){
         bent = angular.element(bent);
         bent.modal("show");
     }
+    $scope.showf=function(){
+        var free = document.getElementById("free");
+        free = angular.element(free);
+        free.modal("show");
+        console.log("free");
+    }
 
     $scope.paidworkshops = function(){
         var q = document.getElementById("paidworkshops");
