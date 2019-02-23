0a385bbed92826b7a4f9090a3b0ebdd7b2253096
vamsivammu <vammuvamsi64@gmail.com>
2019-02-23 16:58:24 +0530

    small changes

--- inserttest.php
+++ inserttest.php
@@ -3,7 +3,7 @@
 require 'connection.php';
 $resp= array();
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $stmt1=$con1->prepare("INSERT INTO `user2019`(`firstname`, `phone`, `college`, `email`, `hash_password`, `conf_code`, `gender`) VALUES(?,?,?,?,?,?,?);");
+    $stmt1=$con1->prepare("INSERT INTO user2019(firstname, phone,college, email, hash_password, conf_code, gender) VALUES(?,?,?,?,?,?,?);");
     $stmt1->bind_param("sssssss",$name,$phone,$college,$email1,$hash_password,$conf_code,$gender);
     $name = "vamsi";
     $phone = "9999999999";
