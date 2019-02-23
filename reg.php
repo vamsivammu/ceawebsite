21653d4d98bc9926bde863113bfd5419f1fdc5cc
vamsivammu <vammuvamsi64@gmail.com>
2019-02-23 18:41:59 +0530

    small changes

--- reg.php
+++ reg.php
@@ -42,22 +42,22 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         //$stmt1->execute();
         if($stmt1->execute()){
             $resp['status']=1;
-            // $sub="Welcome to CEA 2019";
-			// 		$msg4 = '<html><body><div id="email" style="font-family: century gothic;font-size: 16px;">
-			// 		<h2>Hi '.$name.',</h2>
-			// 		<p>Greetings from CEA-2019 team.</p>
-			// 		<p>We are delighted to have you as a registered member.</p>
-			// 		<p>Visit the CEA website and login to find all the details about the CEA Fest 2019. </p>
-			// 		<p>Please click on the link below to verify your email-id.</p>
-			// 		<a href="http://ceaiitm.org/2019/ceawebsite/loginverify?emai='.$email.'&confcode='.$conf_code.'" target="_blank" style="padding: 5px 20px;background: #28b7ed;margin: 20px 40px;color: white;font-size: 25px;text-decoration: none;border-radius:5px;">Verify</a>
-			// 		<p style="font-size:12px;">Please ignore this mail if you did not sign up for CEA.</p>					
-			// 		<p>Thank you,<br/>
-			// 		CEA 2019 Team</p>
-			// 		</div></body></html>';
-			// 		$headers = "From:CEA Web Operations <webops@ceaiitm.org>\r\n";
-			// 		$headers .= "MIME-Version: 1.0\r\n";
-			// 		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-			// 		//mail($email,$sub,$msg4,$headers);
+            $sub="Welcome to CEA 2019";
+					$msg4 = '<html><body><div id="email" style="font-family: century gothic;font-size: 16px;">
+					<h2>Hi '.$name.',</h2>
+					<p>Greetings from CEA-2019 team.</p>
+					<p>We are delighted to have you as a registered member.</p>
+					<p>Visit the CEA website and login to find all the details about the CEA Fest 2019. </p>
+					<p>Please click on the link below to verify your email-id.</p>
+					<a href="http://ceaiitm.org/2019/ceawebsite/loginverify?emai='.$email.'&confcode='.$conf_code.'" target="_blank" style="padding: 5px 20px;background: #28b7ed;margin: 20px 40px;color: white;font-size: 25px;text-decoration: none;border-radius:5px;">Verify</a>
+					<p style="font-size:12px;">Please ignore this mail if you did not sign up for CEA.</p>					
+					<p>Thank you,<br/>
+					CEA 2019 Team</p>
+					</div></body></html>';
+					$headers = "From:CEA Web Operations <webops@ceaiitm.org>\r\n";
+					$headers .= "MIME-Version: 1.0\r\n";
+					$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+					mail($email,$sub,$msg4,$headers);
                     
                 }
 
