ed5e8b4f5985cbf6b5e31cc70a8d370cac593be4
vamsivammu <vammuvamsi64@gmail.com>
2019-02-23 18:14:01 +0530

    svs

--- inserttest.php
+++ inserttest.php
@@ -12,9 +12,13 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     // $hash_password=sha1("V@ms!3235");
     // $conf_code= md5(uniqid(rand()));
     // $gender = "M";
-   $stmt1 = $con->prepare("INSERT INTO testinsert(name) VALUES(?);");
-   $stmt1->bind_param("s",$name);
-   $name   = "vamsi";
+   $stmt1 = $con->prepare("INSERT INTO testinsert(name,email,phone,college) VALUES(?,?,?,?);");
+   $stmt1->bind_param("ssss",$name,$email,$phone,$co);
+   $name = "vamsi";
+    $phone = "9999999999";
+    $college = "test";
+    $email= "test@test.testestttttttt";
+    
     if($stmt1->execute()){
         $resp['status'] = 1;
     }else{
