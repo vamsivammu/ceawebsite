fd1d6423fa35dd35890e016131c5a1cf8a0f6687
vamsivammu <vammuvamsi64@gmail.com>
2019-02-23 17:38:19 +0530

    sma

--- inserttest.php
+++ inserttest.php
@@ -3,16 +3,18 @@
 require 'connection.php';
 $resp= array();
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $stmt1=$con1->prepare("INSERT INTO testinsertt(firstname, phone,college, email, hash_password, conf_code, gender) VALUES(?,?,?,?,?,?,?);");
-    $stmt1->bind_param("sssssss",$name,$phone,$college,$email1,$hash_password,$conf_code,$gender);
-    $name = "vamsi";
-    $phone = "9999999999";
-    $college = "test";
-    $email1= "test@test.testestttttttt";
-    $hash_password=sha1("V@ms!3235");
-    $conf_code= md5(uniqid(rand()));
-    $gender = "M";
-   
+    // $stmt1=$con1->prepare("INSERT INTO testinsertt(firstname, phone,college, email, hash_password, conf_code, gender) VALUES(?,?,?,?,?,?,?);");
+    // $stmt1->bind_param("sssssss",$name,$phone,$college,$email1,$hash_password,$conf_code,$gender);
+    // $name = "vamsi";
+    // $phone = "9999999999";
+    // $college = "test";
+    // $email1= "test@test.testestttttttt";
+    // $hash_password=sha1("V@ms!3235");
+    // $conf_code= md5(uniqid(rand()));
+    // $gender = "M";
+   $stmt1 = $con->prepare("INSERT INTO testinsert(name) VALUES(?);");
+   $stmt->bind_param("s",$name);
+   $name   = "vamsi";
     if($stmt1->execute()){
         $resp['status'] = 1;
     }else{
